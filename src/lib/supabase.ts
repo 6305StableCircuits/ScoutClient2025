42c52eef50bc82deece1a8ec2c73141a0d350ebc
AAflip <jedidjm2008@gmail.com>
2025-01-27 17:43:52 -0800

    ooga booga?

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,8 @@
-import type { RequestHandler } from '../routes/supabase/$types';
-import {json} from '@sveltejs/kit'
-import * as fs from 'fs';
+// import { UNFUN_API_KEY } from '$env/static/private';
 import { createClient } from '@supabase/supabase-js'
-import * as dotenv from 'dotenv'
+import dotenv from 'dotenv';
 dotenv.config();
 
 const supabaseUrl = 'https://ujpszjmkuhaknmzevzxg.supabase.co';
 const supabaseKey = process.env.UNFUN_API_KEY;
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, process.env.FUN_API_KEY!);
