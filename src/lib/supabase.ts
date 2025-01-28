9768ce720702c0452f0aa7bcf75fb8874262fd88
AAflip <jedidjm2008@gmail.com>
2025-01-27 18:08:02 -0800

    i dont know man

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,7 @@
-// import { UNFUN_API_KEY } from '$env/static/private';
 import { createClient } from '@supabase/supabase-js'
 import dotenv from 'dotenv';
 dotenv.config();
 
 const supabaseUrl = 'https://ujpszjmkuhaknmzevzxg.supabase.co';
-const supabaseKey = process.env.UNFUN_API_KEY;
-export const supabase = createClient(supabaseUrl, process.env.FUN_API_KEY!);
+const supabaseKey = process.env.FUN_API_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey!);
