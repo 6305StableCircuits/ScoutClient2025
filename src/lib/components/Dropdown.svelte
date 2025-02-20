fc0ede631e78c5690360299d92ddfcbbf9770f61
Koisu <koisuspotify@gmail.com>
2025-02-19 20:07:13 -0800

    added dropdown menu

---
+++ src/lib/components/Dropdown.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    let {options, current_value = $bindable()} : {options: string[], current_value: any} = $props()
+
+</script>
+
+<select class="border-secondary border-8 rounded-[6px] bg-gray-100 text-GrayedOutText w-[173px] h-[66px]" bind:value={current_value}>
+    {#each options as option }
+        <option value={option}>{option}</option>
+    {/each}
+</select>
+
+
