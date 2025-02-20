df9f3d222da1d9972b0a73b8d2ace575b1054ecd
Koisu <koisuspotify@gmail.com>
2025-02-19 17:17:44 -0800

    added new button

---
+++ src/lib/components/NewButton.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { ButtonSizes, type ButtonBgColors } from '$lib/ButtonStyles';
+	import type { MouseEventHandler } from 'svelte/elements';
+	let {
+		text,
+		func,
+		colour,
+        size = ButtonSizes.Longest
+	}: {
+		text: string;
+		func: MouseEventHandler<HTMLButtonElement>;
+		colour: ButtonBgColors;
+        size?: ButtonSizes;
+	} = $props();
+	$inspect(text);
+	$inspect(colour);
+</script>
+
+<button
+	onclick={func}
+	class="{colour} {size} text-Text button "
+>
+	{text}
+</button>
