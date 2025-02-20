87ff09d67cee63937d24086290c2619f654c7672
Koisu <koisuspotify@gmail.com>
2025-02-19 17:12:53 -0800

    added buttonStyles.ts

---
+++ src/lib/ButtonStyles.ts
@@ -0,0 +1,18 @@
+export enum ButtonBgColors {
+	Primary = 'bg-primary',
+	Secondary = 'bg-secondary',
+	Win = 'bg-WinGreen',
+	Lose = 'bg-LoseRed',
+	Submit = 'bg-SubmitBlue',
+	ExtraBlue = 'bg-ExtraBlue',
+	ExtraGreen = 'bg-ExtraGreen',
+    ExtraLightBlue = "bg-ExtraLightBlue",
+    ExtraOrange = "bg-ExtraOrange"
+}
+
+export enum ButtonSizes{
+	Longest = "w-[402px] h-[94px]",
+	SlightlyLessLong = "w-[323px] h-[94px]",
+	Short = "w-[181px] h-[94px]"
+}
+
