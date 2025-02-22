1f989c70b859ecb8dea7b18dc3346e6f08bd780b
Koisu <koisuspotify@gmail.com>
2025-02-22 12:07:28 -0800

    duringScoutroute

---
+++ src/routes/duringScout/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+    
+    let scoutName = $page.url.searchParams.get("name");
+</script>
+
+<p bind:textContent={scoutName} contenteditable="true"></p>
