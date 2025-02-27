cd789e4696110fd9db7803f1edf422254ca0e8ab
Koisu <koisuspotify@gmail.com>
2025-02-26 17:54:32 -0800

    extra colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,7 @@ export default {
             ExtraBlue: "#3682C5",
             ExtraGreen: "rgba(54, 197, 68, 0.52)",
             ExtraOrange: "#FF8559",
+            ExtraSlightlyDarkOrange: "#bf6443",
             ExtraLightBlue: "#31A8C0",
             GrayedOutText: "rgba(91, 90, 90, 1)",
             ...colors
