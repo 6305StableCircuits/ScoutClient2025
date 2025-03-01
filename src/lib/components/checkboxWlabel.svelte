81c12f33bac04f507f14ad79a1b34014b54b5c56
Koisu <koisuspotify@gmail.com>
2025-02-28 17:06:03 -0800

    checkbox with AH

--- src/lib/components/checkboxWlabel.svelte
+++ src/lib/components/checkboxWlabel.svelte
@@ -4,25 +4,24 @@
     
     interface Props{
         label: string;
-        friend_name: string;
         values: number;
         value: number
     }
-    let {label, values = $bindable(), friend_name, value} : Props  = $props();
+    let {label, values = $bindable(), value} : Props  = $props();
 
     let id: string = uuidv4()
 </script>
 
 
-<div class="flex justify-center text-4xl font-[700]">
+<div class="flex justify-center text-4xl font-[700] m-4">
     <label for={id} class="mr-3">{label}</label>
     <input
-      type="radio"
+      type="checkbox"
       bind:group={values}
       {value}
       class="w-16 h-16 lg:w-10 lg:h-10 accent-secondary rounded-lg"
       {id}
-      name={friend_name ?? id}
+      name={id}
     />
 </div>
 
