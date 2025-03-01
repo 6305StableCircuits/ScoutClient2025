fcc04889206b8fae7543f4ff826b971f2390c2ca
Koisu <koisuspotify@gmail.com>
2025-02-28 16:59:53 -0800

    radial buton thing

--- src/lib/components/radialWlabel.svelte
+++ src/lib/components/radialWlabel.svelte
@@ -3,32 +3,27 @@
 
     
     interface Props{
-        radial_button: boolean;
         label: string;
         friend_name: string;
         values: number;
         value: number
     }
-    let {radial_button, label, values = $bindable(), friend_name, value} : Props  = $props();
+    let {label, values = $bindable(), friend_name, value} : Props  = $props();
 
     let id: string = uuidv4()
 </script>
 
 
-<div class="flex justify-center">
-    <label for={id} class="text-3xl font-[700] mr-3">{label}</label>
-    {#if radial_button}
+<div class="flex justify-center text-4xl font-[700]">
+    <label for={id} class="mr-3">{label}</label>
     <input
       type="radio"
       bind:group={values}
       {value}
-      class="w-10 h-10 accent-secondary rounded-lg"
+      class="w-16 h-16 lg:w-10 lg:h-10 accent-secondary rounded-lg"
       {id}
       name={friend_name ?? id}
     />
-    {:else}
-
-    {/if}
 </div>
 
 
