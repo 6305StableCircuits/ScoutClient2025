dc921d2b2121f58d1e0e120b938600aeaac19d45
AAflip <jedidjm2008@gmail.com>
2025-11-05 18:45:38 -0800

    update stuff

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const supabaseUrl = 'https://ujpszjmkuhaknmzevzxg.supabase.co';
+const supabaseUrl = 'https://kotupzmdqdmyhbqqvgiv.supabase.co';
 const supabaseKey = process.env.FUN_API_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey!);
