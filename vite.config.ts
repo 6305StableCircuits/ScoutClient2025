196fad9d81efaadbcfeed0f56576eb5d3681e4a9
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2025-11-06 19:42:51 -0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [sveltekit(), enhancedImages()],
+    plugins: [enhancedImages(), sveltekit()],
     build: {
         minify: 'terser'
     }
